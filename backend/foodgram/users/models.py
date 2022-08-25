a6448338243d9df0ac0e15549ad1e04514474280
Александр Мамонов <Alex.A.Mamonov@yandex.ru>
2022-08-25 11:42:19 +0300

    1.0

---
+++ backend/foodgram/users/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+    email = models.EmailField(
+        verbose_name='E-mail',
+        unique=True,
+        max_length=254,
+        blank=False
+    )
+    first_name = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=150, blank=False)
